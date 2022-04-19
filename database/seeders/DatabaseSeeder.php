28434885850682ab71a93b346342d48fd04951d5
iamseryous <44864926+YouuSer@users.noreply.github.com>
2022-04-19 02:14:42 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,34 +31,34 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\User::factory(5)->create();
 
-        DB::table('videos')->insert([
-            'title' => 'Mer',
-            'path' => '/videos/8lUQoWxWkiP66LBeQcjJ9WPUqwT3BlJ743zKIQOy.mp4',
-            'user_id' => 2,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('videos')->insert([
-            'title' => 'Plage',
-            'path' => '/videos/wV4r0n0hmCkR6KssWVfVPelPKkiif8m5QhQVD9lG.mp4',
-            'user_id' => 3,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('videos')->insert([
-            'title' => 'Desert',
-            'path' => '/videos/kBHzA9OjV4AgFjCwpSPFPoWzXxZt88kh5FSeThUN.mp4',
-            'user_id' => 4,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('videos')->insert([
-            'title' => 'Volcan',
-            'path' => '/videos/ZrV0PqHBrBDogKe4CLmf9F4IHjsPQ2LFHhoMFTep.mp4',
-            'user_id' => 5,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('videos')->insert([
+        //     'title' => 'Mer',
+        //     'path' => '/videos/8lUQoWxWkiP66LBeQcjJ9WPUqwT3BlJ743zKIQOy.mp4',
+        //     'user_id' => 2,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // DB::table('videos')->insert([
+        //     'title' => 'Plage',
+        //     'path' => '/videos/wV4r0n0hmCkR6KssWVfVPelPKkiif8m5QhQVD9lG.mp4',
+        //     'user_id' => 3,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // DB::table('videos')->insert([
+        //     'title' => 'Desert',
+        //     'path' => '/videos/kBHzA9OjV4AgFjCwpSPFPoWzXxZt88kh5FSeThUN.mp4',
+        //     'user_id' => 4,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // DB::table('videos')->insert([
+        //     'title' => 'Volcan',
+        //     'path' => '/videos/ZrV0PqHBrBDogKe4CLmf9F4IHjsPQ2LFHhoMFTep.mp4',
+        //     'user_id' => 5,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
         \App\Models\Comment::factory(10)->create();
     }
